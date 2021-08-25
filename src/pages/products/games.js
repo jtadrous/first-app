46c66bcb12237ef9627a52b4fd4a51d0bd00bade
Julie Tadrous <35301030+jtadrous@users.noreply.github.com>
2021-08-25 16:53:33 -0400

    Uploading new files

---
+++ src/pages/products/games.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const GamesPage = () => {
+    return (
+        <React.Fragment>
+            <h1>Games Page</h1>
+        </React.Fragment>
+    );
+};
+
+export default GamesPage;
+
+//the link to this page would be http://localhost:8000/products/games
---
+++ src/pages/products/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ProductsPage = () => {
+    return (
+        <React.Fragment>
+            <h1>Products Page</h1>
+        </React.Fragment>
+    );
+};
+
+export default ProductsPage;
+
+//the link to this page would be http://localhost:8000/products bc it is the index page of the folder
